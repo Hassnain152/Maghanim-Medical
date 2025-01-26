f22fa5dfcc730d552a4e27ba27c4f5e21c519016
hassnain zafar <hassnainzafar152@gmail.com>
2025-01-26 13:29:29 +0300

    Normalize line endings

---
+++ src/AllComp/Navbar.js
@@ -0,0 +1,122 @@
+import React, { useState } from "react";
+import logo from "../Assets/logoo.png";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+function Navbar() {
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+  const toggleNavbar = () => {
+    setIsNavCollapsed(!isNavCollapsed);
+  };
+
+  return (
+    <div className="App">
+      <nav
+        className="navbar navbar-expand-lg navbar-light fixed-top"
+        style={{ backgroundColor: "white", height: "auto" }}
+      >
+        <div className="container-fluid">
+          {/* Logo */}
+          <a className="navbar-brand" href="#top">
+            <img
+              className="logoo"
+              src={logo}
+              alt="Logo"
+              style={{
+                width: "130px",
+                height: "auto",
+                maxHeight: "85px",
+              }}
+            />
+          </a>
+
+          {/* Toggler Button */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded={!isNavCollapsed}
+            aria-label="Toggle navigation"
+            onClick={toggleNavbar}
+          >
+            <span
+              className={`navbar-toggler-icon ${
+                isNavCollapsed ? "" : "btn-close"
+              }`}
+              aria-hidden="true"
+            ></span>
+          </button>
+
+          {/* Navbar Links */}
+          <div
+            className={`collapse navbar-collapse ${isNavCollapsed ? "" : "show"}`}
+            id="navbarNav"
+          >
+            <ul className="navbar-nav ms-auto">
+              <li className="items">
+                <a
+                  className="nav-link"
+                  aria-current="page"
+                  href="#top"
+                  style={{ fontSize: 20, color: "#4e382a", padding: "15px" }}
+                >
+                  HOME
+                </a>
+              </li>
+              <li className="items">
+                <a
+                  className="nav-link"
+                  href="#Aboutus"
+                  style={{ fontSize: 20, color: "#4e382a", padding: "15px" }}
+                >
+                  ABOUT US
+                </a>
+              </li>
+              <li className="items">
+                <a
+                  className="nav-link"
+                  href="#Client"
+                  style={{ fontSize: 20, color: "#4e382a", padding: "15px" }}
+                >
+                  OUR CLIENTS
+                </a>
+              </li>
+              <li className="items">
+                <a
+                  className="nav-link"
+                  href="#offer"
+                  style={{ fontSize: 20, color: "#4e382a", padding: "15px" }}
+                >
+                  SERVICES
+                </a>
+              </li>
+              <li className="items">
+                <a
+                  className="nav-link"
+                  href="#projects"
+                  style={{ fontSize: 20, color: "#4e382a", padding: "15px" }}
+                >
+                  PROJECTS
+                </a>
+              </li>
+              <li className="items">
+                <a
+                  className="nav-link"
+                  href="#Contactus"
+                  style={{ fontSize: 20, color: "#4e382a", padding: "15px" }}
+                >
+                  CONTACT US
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
