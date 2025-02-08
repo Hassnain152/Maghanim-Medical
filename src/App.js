7e4e498d2fa5749fc88b225aec627c0bf643d9da
hassnain zafar <hassnainzafar152@gmail.com>
2025-02-09 02:09:43 +0300

    error

--- src/App.js
+++ src/App.js
@@ -12,6 +12,10 @@ import Projects from './AllComp/Projects';
 import Contactus from './AllComp/Contactus';
 import Footer from './AllComp/Footer';
 import Form from "./AllComp/Form"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
+
+
 
 
 
@@ -20,16 +24,30 @@ function App() {
 
   
   return (
-    <div className="App">
-    <>
-    <Router>
-      <Routes>
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 text-center">
+   <FontAwesomeIcon size="2x" icon={faCircleExclamation} />
+    <h1 className="text-5xl font-bold text-red-600">404</h1>
+    <h2 className="text-2xl font-semibold mt-2">Oops! Something went wrong.</h2>
+    <p className="text-lg text-gray-600 mt-2">
+      Please contact your developer to resolve this issue.
+    </p>
+    <a href="/" className="mt-6 px-6 py-3 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600">
+      Go Back Home
+    </a>
+  </div>
+  );
+}
+
+
+
+    {/* <Router>
+      <Routes> */}
         {/* Main Home Page */}
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Home />} /> */}
 
         {/* This route will show only the Projects page */}
-        <Route path="/projects-only" element={<Projects />} />
-      </Routes>
+        {/* <Route path="/projects-only" element={<Projects />} /> */}
+      {/* </Routes>
     </Router>
 
     <Navbar/>
@@ -38,10 +56,10 @@ function App() {
     <Aboutus/>
     <Contactus/>
     <Form/>
-    <Footer/>
-    </>
-    </div>
-  );
-}
+    <Footer/> */}
+//     </>
+//     </div>
+//   );
+// }
 
 export default App;
